85f3dba99fb4576610316244a5373311896f3146
sanyabeast <a.gvrnsk@gmail.com>
2018-05-22 12:17:15 +0300

    Major functionality

--- klasstools.js
+++ klasstools.js
@@ -1,5 +1,14 @@
 "use strict";
-define(function(){
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(factory);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = factory(true);
+    } else {
+        window.$Class = factory();
+        window.$Interface = window.$Class.$Interface;
+    }
+}(this, function(){
 
 	/**Toolchain*/
 	var Toolchain = function(){};
@@ -21,12 +30,21 @@ define(function(){
 
 	/**Interface implementation*/
 	var $Interface = function(params){
-		this.params = params;
+		this.params = params || {};
 	};
 
 	$Interface.prototype = {
-		__validateClass : function($class){
+		__validateClass : function($constructor){
+			var result = true;
 
+			_.loop(this.params, function(type, name){
+				if (typeof $constructor.prototype[name] != type){
+					result = false;
+					console.error("The implementation of does not match the interface: `" + name + "` is not a `" + type + "`, it's a `" + typeof $constructor.prototype[name] + "`");
+				}
+			});
+
+			return result;
 		}
 	};
 
@@ -48,45 +66,51 @@ define(function(){
 		if (!(interfaces instanceof Array)){
 			$prototype = interfaces;
 			interfaces = null;
-			$super = null;
-
 		}
 
-		var $class = this.__createClass(name, $super, interfaces, $prototype);
+		console.log(name, $prototype, $super, interfaces);
+
+		var $class = this.__createClass(name, $prototype, $super, interfaces);
 
 		return $class;
 	};
 
 	$Class.prototype = {
-		__createClass : function(name, $super, interfaces, $prototype){
-			var $construcor;
+		__createClass : function(name, $prototype, $super, interfaces){
+			var $constructor;
 
-			if (typeof $prototype.$construcor == "function"){
-				$construcor = $prototype.$construcor;
-				delete $prototype.$construcor;
+			if (typeof $prototype.$constructor == "function"){
+				$constructor = $prototype.$constructor;
+				delete $prototype.$constructor;
 			} else {
-				$construcor = function(){};
+				$constructor = function(){};
 			}
 
-			console.log($construcor);
+			$constructor = this.__addSuper($constructor);
 
-			$construcor = this.__addSuperConstrucor($construcor);
+			$constructor = this.__renameFunction($constructor, name);
 
-			$construcor = this.__renameFunction($construcor, name);
+			this.__setupProto($constructor, $prototype, $super, interfaces);
 
-			this.__setupProto($construcor, $prototype, $super, interfaces);
+			if ($constructor.$interfaces){
+				_.loop($constructor.$interfaces, function($interface, index){
+					if ($interface instanceof $Interface){
+						$interface.__validateClass($constructor);
+					}
+				});
+			}
 
-			return $construcor;
+			return $constructor;
 
 		},	
-		__setupProto : function($construcor, $prototype, $super, interfaces){
+		__setupProto : function($constructor, $prototype, $super, interfaces){
 			if ($super){
-				this.loop($super.$prototype, function(token, name){
-					this.__defineProperty($construcor.prototype, name, token);
+				_.loop($super.$prototype, function(token, name){
+					this.__defineProperty($constructor.prototype, name, token);
 				}, this);	
 
-				this.__defineProperty($construcor.prototype, "$super", {
-					value : $super.prototype,
+				this.__defineProperty($constructor.prototype, "$super", {
+					value : $super,
 					enumerable : false,
 					writable : false,
 					configurable : false
@@ -94,7 +118,7 @@ define(function(){
 			}	
 
 			if (interfaces){
-				this.__defineProperty($construcor.prototype, "$interfaces", {
+				this.__defineProperty($constructor, "$interfaces", {
 					value : interfaces,
 					enumerable : false,
 					writable : false,
@@ -102,45 +126,77 @@ define(function(){
 				});
 			}
 
-			this.__defineProperty($construcor.prototype, "$prototype", {
+			this.__defineProperty($constructor, "$prototype", {
 				value : $prototype,
 				enumerable : false,
 				writable : false,
 				configurable : false
 			});
 
-			console.log($prototype);
 
-			_.loop($prototype, function(token, name){
-				console.log(name);
-				this.__defineProperty($construcor.prototype, name, token);
+			_.loop($prototype, function(token, name, list){
+				if (typeof token == "function"){
+					token = list[name] = this.__addSuper(token, name);
+				} else if (typeof token.value == "function"){
+					token.value = this.__addSuper(token.value, name);
+				}
+
+				this.__defineProperty($constructor.prototype, name, token);
 			}, this);	
 
-			this.__defineProperty($construcor.prototype, "$construcor", {
-				value : $construcor,
+			this.__defineProperty($constructor, "$constructor", {
+				value : $constructor,
+				enumerable : false,
+				writable : false,
+				configurable : false
+			});
+
+			this.__defineProperty($constructor, "$name", {
+				value : name,
+				enumerable : false,
+				writable : false,
+				configurable : false
+			});
+
+			this.__defineProperty($constructor, "extend", {
+				value : this.__extend.bind(this, $constructor),
 				enumerable : false,
 				writable : false,
 				configurable : false
 			});
 		},
+		__extend : function($super, name, interfaces, $prototype){
+			return new Klass(name, $super, interfaces, $prototype);
+		},	
 		__renameFunction : function(func, name){
 			return eval(["var ", name, "=", func.toString(), ";", name, ";"].join(""));
 		},	
-		__addSuperConstrucor(func){
+		__addSuper(func, name){
 			var stringified = func.toString();
 			var start = stringified.split("{")[0];
 			var body = stringified.split("{")[1];
 
 			body = body.split("}")[0];
 
-			body = [[
-				  "var args = Array.prototype.slice.call(arguments);"
-				, "	  var $super = function(args){",
-				, "	  if (this.$super && typeof this.$super.$constructor == \"function\"){"
-				, "	  	   this.$super.$constructor.apply(this, args);"
-				, "	  }"
-				, "}.bind(this, args)"
-			].join(""), body].join("");	
+			if (!name){
+				body = [[
+					  "var args = Array.prototype.slice.call(arguments);"
+					, "var $super = function(args){",
+					, "	  if (this.$super && typeof this.$super.$constructor == \"function\"){"
+					, "	  	   this.$super.$constructor.apply(this, args);"
+					, "	  }"
+					, "}.bind(this, args)"
+				].join(""), body].join("");	
+			} else {
+				body = [[
+					  "var args = Array.prototype.slice.call(arguments);"
+					, "var $super = function(args){",
+					, "	  if (this.$super && this.$super.$prototype && typeof this.$super.$prototype[\"" + name + "\"] == \"function\"){"
+					, "	  	   this.$super.$prototype[\"" + name + "\"].apply(this, args);"
+					, "	  }"
+					, "}.bind(this, args)"
+				].join(""), body].join("");	
+			}			
 
 			return eval(["var $constructor = ", start, "{", body, "}", ";$constructor;"].join(""));		
 
@@ -161,6 +217,8 @@ define(function(){
 		},
 	};
 
-	return $Class;
+	$Class.$Interface = $Interface;
 
-});
+	return $Class;
+    
+}));
