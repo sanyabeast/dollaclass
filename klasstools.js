47b936968e9e4fc3ecceb62f4d7dfa40094ab15e
sanyabeast <a.gvrnsk@gmail.com>
2018-05-23 16:00:03 +0300

    Changed the way of adding super to keep closure alive

--- klasstools.js
+++ klasstools.js
@@ -70,9 +70,7 @@
 				$constructor = function(){};
 			}
 
-			$constructor = this.__addSuper($constructor);
-
-			$constructor = this.__renameFunction($constructor, name);
+			$constructor = this.__addSuper($constructor, name, true);
 
 			this.__setupProto(name, $constructor, $prototype, $superConstructor, interfaces);
 
@@ -124,9 +122,9 @@
 
 			_.loop($prototype, function(token, name, list){
 				if (typeof token == "function"){
-					token = list[name] = this.__addSuper(token, name);
+					token = list[name] = this.__addSuper(token, name, false);
 				} else if (typeof token.value == "function"){
-					token.value = this.__addSuper(token.value, name);
+					token.value = this.__addSuper(token.value, name, false);
 				}
 
 				if (token.static === true){
@@ -161,37 +159,40 @@
 		__extend : function($superConstructor, name, interfaces, $prototype){
 			return new Klass(name, $superConstructor, interfaces, $prototype);
 		},	
-		__renameFunction : function(func, name){
-			return eval(["var ", name, "=", func.toString(), ";", name, ";"].join(""));
-		},	
-		__addSuper(func, name){
-			var stringified = func.toString();
-			var start = stringified.split("{")[0];
-			var body = stringified.substring(stringified.indexOf("{") + 1, stringified.lastIndexOf("}"));
+		__addSuper(func, name, isConstructor){
+			var wrapped = function(){
+				var args = Array.prototype.slice.call(arguments);
+				var $super;
 
-			start = start.match(/\(.*?\)/)[0];
+				if (isConstructor){
+					$super = function(args){
+						if (this.$super && typeof this.$super.$constructor == "function"){
+							this.$super.$constructor.apply(this, args);
+						}
+					}.bind(this, args);
+				} else {
+					$super = function(args){
+						if (this.$super && this.$super.$prototype && typeof this.$super.$prototype[name] == "function"){
+							this.$super.$prototype[name].apply(this, args);
+						}
+					}.bind(this, args);
+				}
 
-			if (!name){
-				body = [[
-					  "var args = Array.prototype.slice.call(arguments);"
-					, "var $super = function(args){",
-					, "	  if (this.$super && typeof this.$super.$constructor == \"function\"){"
-					, "	  	   this.$super.$constructor.apply(this, args);"
-					, "	  }"
-					, "}.bind(this, args);"
-				].join(""), body].join("");	
-			} else {
-				body = [[
-					  "var args = Array.prototype.slice.call(arguments);"
-					, "var $super = function(args){",
-					, "	  if (this.$super && this.$super.$prototype && typeof this.$super.$prototype[\"" + name + "\"] == \"function\"){"
-					, "	  	   this.$super.$prototype[\"" + name + "\"].apply(this, args);"
-					, "	  }"
-					, "}.bind(this, args);"
-				].join(""), body].join("");	
-			}			
+				var newArgs = args.slice();
+				newArgs.push($super);
 
-			return eval(["var $constructor = function", start, "{", body, "}", ";$constructor;"].join(""));		
+				if (true || !isConstructor){
+					return func.apply(this, newArgs);
+				} else {
+					return new (Function.prototype.bind.apply(func, newArgs));
+				}
+
+			};
+
+			wrapped = eval(["var ", name, "=", wrapped.toString(), ";", name, ";"].join(""));
+			wrapped.isSuper = true;
+
+			return wrapped;
 
 		},
 		__checkInterfacesImplementation : function(){
@@ -264,7 +265,6 @@
 		__createClass : $Class.prototype.__createClass,
 		__setupProto : $Class.prototype.__setupProto,
 		__extend : $Class.prototype.__extend,
-		__renameFunction : $Class.prototype.__renameFunction,
 		__addSuper : $Class.prototype.__addSuper,
 		__checkInterfacesImplementation : $Class.prototype.__checkInterfacesImplementation,
 		__defineProperty : $Class.prototype.__defineProperty,
